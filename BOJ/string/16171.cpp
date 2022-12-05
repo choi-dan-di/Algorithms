5a31f9aff2af277752fa88b20d2ab358c365d2a2
choi-dan-di <tanvely@kakao.com>
2022-12-05 21:32:42 +0900

    upload boj-16171

---
+++ BOJ/string/16171.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <regex>
+using namespace std;
+string s, k;
+char s[101], k[101];
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    // 나는 친구가 적다 (Small)
+    cin >> s >> k;
+    s = regex_replace(s, regex("[0-9]"), "");
+    if(strstr(s.c_str(), k.c_str())) cout << "1";
+    else cout << "0";
+    return 0;
+}

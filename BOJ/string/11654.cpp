1472770020710d9da6aabb38d298da9661a58338
choi-dan-di <tanvely@kakao.com>
2022-12-12 21:55:29 +0900

    upload boj-11654

---
+++ BOJ/string/11654.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    // 아스키 코드
+    char c;
+    cin >> c;
+    cout << (int)c;
+    return 0;
+}

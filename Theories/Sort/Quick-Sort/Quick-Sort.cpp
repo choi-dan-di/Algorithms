9a7de4d4e9819220716124baf9f92f2e28d08592
choi-dan-di <tanvely@kakao.com>
2023-02-09 17:07:40 +0900

    upload algorithm/quick-sort

--- Theories/Sort/Quick-Sort/Quick-Sort.cpp
+++ Theories/Sort/Quick-Sort/Quick-Sort.cpp
@@ -8,7 +8,50 @@ using namespace std;
 
 // 퀵 정렬
 
+int Partition(vector<int>& v, int left, int right)
+{
+    int pivot = v[left];
+    int low = left + 1;
+    int high = right;
+
+    while (low <= high)
+    {
+        while (low <= right && pivot >= v[low])
+            low++;
+
+        while (high >= left + 1 && pivot <= v[high])
+            high--;
+
+        if (low < high)
+            swap(v[low], v[high]);
+    }
+
+    swap(v[left], v[high]);
+    return high;
+}
+
+// [5][1][3][7][9][2][4][6][8]
+void QuickSort(vector<int>& v, int left, int right)
+{
+    if (left > right)
+        return;
+
+    int pivot = Partition(v, left, right);
+    QuickSort(v, left, pivot - 1);
+    QuickSort(v, pivot + 1, right);
+}
+
 int main()
 {
-    
+    vector<int> v;
+
+    srand(time(0));
+
+    for (int i = 0; i < 50; i++)
+    {
+        int randValue = rand() % 100;
+        v.push_back(randValue);
+    }
+
+    QuickSort(v, 0, v.size() - 1);
 }

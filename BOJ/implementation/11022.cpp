2ace96b8d543e113e86c2be49ed01b7c8fffc6b5
choi-dan-di <tanvely@kakao.com>
2023-04-02 13:15:10 +0900

    백준 알고리즘 구현 1문제 풀이

---
+++ BOJ/implementation/11022.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    // A+B - 8
+    int n, a, b;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a >> b;
+        cout << "Case #" << i << ": " << a << " + " << b << " = " << a + b << '\n';
+    }
+    return 0;
+}

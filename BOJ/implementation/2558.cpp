241cc6c14f3fe28ccefee211d0c92caee75bcf63
choi-dan-di <tanvely@kakao.com>
2023-04-06 00:20:31 +0900

    백준 알고리즘 구현 1문제 풀이

---
+++ BOJ/implementation/2558.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    // A+B - 2
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << '\n';
+    return 0;
+}

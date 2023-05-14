5c22d42976aaa289efbdf47f099e12f6b9e550d1
choi-dan-di <tanvely@kakao.com>
2023-05-14 20:15:55 +0900

    백준 알고리즘 문자열 1문제 풀이

---
+++ BOJ/string/2743.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    // 단어 길이 재기
+    string s;
+    cin >> s;
+    cout << s.size();
+
+    return 0;
+}

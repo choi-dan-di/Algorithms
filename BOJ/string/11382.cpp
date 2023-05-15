3c792859a77052cb055f394010de8c9fdaa600f7
choi-dan-di <tanvely@kakao.com>
2023-05-15 21:49:34 +0900

    --

---
+++ BOJ/string/11382.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    // 꼬마 정민
+    long long a, b, c;
+    cin >> a >> b >> c;
+    cout << a + b + c;
+
+    return 0;
+}

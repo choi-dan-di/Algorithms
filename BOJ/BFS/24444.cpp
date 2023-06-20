a9dc7f17d2e24456d2373ff9644d0e6a35a6e100
choi-dan-di <tanvely@kakao.com>
2023-06-20 19:58:37 +0900

    백준 알고리즘 BFS 2문제 풀이 - 24444, 24445

---
+++ BOJ/BFS/24444.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int N, M, R, u, v, recent_cnt;
+
+vector<vector<int> > adj;
+vector<bool> visited;
+vector<int> cnt;
+queue<int> q;
+
+void bfs(int vertex);
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    // 알고리즘 수업 - 너비 우선 탐색 1
+    cin >> N >> M >> R;
+    
+    adj.resize(N);
+    visited.resize(N);
+    cnt.resize(N);
+
+    // 간선
+    for (int i = 0; i < M; ++i)
+    {
+        cin >> u >> v;
+        adj[u - 1].push_back(v - 1);
+        adj[v - 1].push_back(u - 1);
+    }
+
+    // sort
+    for (auto& edge : adj)
+        sort(edge.begin(), edge.end());
+
+    bfs(R - 1);
+
+    for (int& i : cnt)
+        cout << i << '\n';
+
+    return 0;
+}
+
+void bfs(int vertex)
+{
+    visited[vertex] = true;
+
+    q.push(vertex);
+
+    while (!q.empty())
+    {
+        int now = q.front();
+        q.pop();
+        cnt[now] = ++recent_cnt;
+        
+        for (auto& next : adj[now])
+        {
+            if (!visited[next])
+            {
+                visited[next] = true;
+                q.push(next);
+            }
+        }
+    }
+}

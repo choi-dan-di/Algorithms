618748e89940b1589667425a28193c9dbfe2a01e
choi-dan-di <tanvely@kakao.com>
2023-06-27 16:41:34 +0900

    BOJ - [우선순위 큐] 최소 힙(No.1927) solved

---
+++ BOJ/data-structure/queue/1927.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int N, x;
+priority_queue<int> pq;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    // 최소 힙
+    cin >> N;
+    while (N--)
+    {
+        cin >> x;
+        if (x == 0)
+        {
+            if (pq.size() == 0)
+            {
+                cout << 0 << '\n';
+                continue;
+            }
+            cout << -pq.top() << '\n';
+            pq.pop();
+            continue;
+        }
+        pq.push(-x);
+    }
+
+    return 0;
+}

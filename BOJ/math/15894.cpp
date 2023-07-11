c068ad479add80f5f1de599758653b3e3f466fac
choi-dan-di <tanvely@kakao.com>
2023-07-11 23:43:39 +0900

    BOJ - [수학] 수학은 체육과목 입니다. (No.15894) solved

---
+++ BOJ/math/15894.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    // 수학은 체육과목 입니다
+    long long n;
+    cin >> n;
+    cout << n * 4;
+    
+    return 0;
+}

4d4204b1a03d451563416e54def8287f67ad0c8f
choi-dan-di <tanvely@kakao.com>
2023-08-17 21:57:31 +0900

    BOJ - [누적 합] 수들의 합 2 (No.2003) solved

---
+++ BOJ/prifix-sum/2003.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    // 수들의 합 2
+    int N, M;
+    int arr[10001] = { 0, };
+    int sumArr[10001] = { 0, };
+    cin >> N >> M;
+    for (int i = 1; i <= N; i++)
+    {
+        cin >> arr[i];
+        sumArr[i] = sumArr[i - 1] + arr[i];
+    }
+
+    int cnt = 0;
+    for (int i = 1; i <= N; i++)
+        for (int j = 1; j <= N; j++)
+            if (sumArr[j] - sumArr[i - 1] == M)
+                cnt++;
+
+    cout << cnt; 
+
+    return 0;
+}

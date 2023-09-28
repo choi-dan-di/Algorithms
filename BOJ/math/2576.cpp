9e3a0dc3c94e021be7e5ecf06cadd315a9e12f81
choi-dan-di <tanvely@kakao.com>
2023-09-28 21:44:24 +0900

    BOJ - [수학] 홀수 (No.2576) solved

---
+++ BOJ/math/2576.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+string me, doctor;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    // 홀수
+    int sum = 0;
+    int minNum = 101;
+    for (int i = 0; i < 7; i++)
+    {
+        int num;
+        cin >> num;
+
+        if (num % 2 == 1)
+        {
+            sum += num;
+            minNum = min(minNum, num);
+        }
+    }
+
+    if (sum == 0)
+    {
+        cout << -1;
+        return 0;
+    }
+
+    cout << sum << '\n' << minNum;
+
+    return 0;
+}
